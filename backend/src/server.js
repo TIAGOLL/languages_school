fe62e17ccab0e17216b8686e22179570ea99859d
Tiago Lima <tiagoepitanga10@gmail.com>
2024-02-27 00:36:31 -0300

    Corrigindo bug CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,13 @@ import { router } from "./routes";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",  
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use(router);
 
 app.listen(3030, () => console.log("Server is running!"));
