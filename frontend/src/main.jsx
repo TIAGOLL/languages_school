ad911c57a547798aef8440ae0a0100325e66fdcd
Tiago Lima <tiagoepitanga10@gmail.com>
2024-03-10 13:34:35 -0300

    Speed Insights Adicionado

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './global.css'
 import './normalize.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div>
+      <SpeedInsights />
       <App />
     </div>
   </React.StrictMode>,
