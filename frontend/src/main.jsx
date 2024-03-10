9461c72926a54eb44727a6723a0307e4a6775e22
Tiago Lima <tiagoepitanga10@gmail.com>
2024-03-10 13:41:34 -0300

    Analytics Adicionado

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './global.css'
 import './normalize.css'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div>
       <SpeedInsights />
+      <Analytics />
       <App />
     </div>
   </React.StrictMode>,
